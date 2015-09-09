62b754f7a2664e55e3e986abe6961300c0190226
Arkadi Listov <arkalis@greeny.cs.tlu.ee>
2015-09-09 12:16:00 +0300

    first lines of code

--- helloworld.php
+++ helloworld.php
@@ -4,3 +4,38 @@
 
 	echo $first_name." ".$last_name;
 ?>
+<br>
+<?php
+
+		$age = 19;
+		
+		//testib loogikat, juhul kui vanus on väiksem
+		//kui 18 siis kirjuta "alaealine", muul juhul "täisealine"
+		
+		//if'i sisse loogikatehe
+		if($age < 18) {
+			//tõene 
+			echo "alaealine";
+			
+		 }	else {
+				//väär
+				echo"täisealine";
+				 }
+				 
+				 
+?>
+<br>
+<?php
+
+    //vastavalt vanusele trükime niimitu korda välja sõna "palju"
+
+	for($i=0;$i < $age; $i = $i +1) {
+	
+	//tegevus mis kordub
+	echo "palju".$i." ";
+	
+	}
+	
+	echo "õnne!";
+
+?>
