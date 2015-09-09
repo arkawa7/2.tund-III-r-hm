8f5ce62a630f9ff25dd3fc4d35906c69d8a4ffaf
Arkadi Listov <arkalis@greeny.cs.tlu.ee>
2015-09-09 13:12:30 +0300

    Handling errors

--- login.php
+++ login.php
@@ -1,7 +1,24 @@
 <?php
 
 	// LOGIN.PHP
-
+	
+	$email_error= "";
+	
+	//echo $_POST["email"];
+	
+	//kontrollime et keegi vajutas input nuppu
+	if($_SERVER["REQUEST_METHOD"] == "POST") {
+		
+		//echo "keegi vajutas nuppu";
+		
+		//kontrollin, et e-post ei ole tühi
+		
+		$email_error = "";
+		
+	if(empty($_POST["email"] ) ) {
+			$email_error = "See vali on kohustuslik";
+		}
+	}
 
 ?>
 <html>
@@ -10,10 +27,12 @@
 </head>
 <body>
 	<h2>Log in</h2>
+	<form action="login.php" method="POST" >
+		<input name="email" placeholder="E-post"> <?php echo $email_error; ?><br><br>
+		<input name="password" type="password" placeholder="Parool"> <br><br>
+		<input type="submit" value="Log in">
+	</form>
 	
-	<input type="email" placeholder="E-post"> <br><br>
-	<input type="password" placeholder="Parool"> <br><br>
-	<input type="submit" value="Log in"
 	<h2>Create user</h2>
 </body>
 
